21393df4aa663c268293f800cbb0985966bc5d18
projects <rune1.rasmussen@gmail.com>
2024-08-01 00:04:37 +1000

    arducam focus implemented

--- cslics_vision_processor/imaging/image_source.py
+++ cslics_vision_processor/imaging/image_source.py
@@ -7,15 +7,42 @@ import numpy
 from typing import Callable
 from logging import Logger
 
+##
+# @brief class IImageSource - interface for image sources
 class ImageSource:
+
+    ##
+    # @brief __init__ - initialises this image source instance.
+    # @param output_length_max : the maximum number of bytes in the image
+    # @param callback_on_frame_raw : the image callback function
+    # @param callback_on_frame_encoded : the image encoding callback function
     def __init__(self, output_length_max: int, callback_on_frame_raw: Callable[[numpy.ndarray], None], callback_on_frame_encoded: Callable[[bytes], None], logger: Logger):
         self.logger: Logger = logger
         self.output_length_max: int = output_length_max
         self.callback_on_frame_raw: Callable[[numpy.ndarray], None] = callback_on_frame_raw
         self.callback_on_frame_encoded: Callable[[bytes], None] = callback_on_frame_encoded
 
-    def capture(self) -> None:
+    ##
+    # @brief get_focal_parameters - gets a list of focal parameters from the image source.
+    # @return list - a list of focal parameters
+    def get_focus(self) -> list:
+        return 
+    
+    ##
+    # @brief set_focus - adjusts the focus of the image source.
+    # @param focal_settings : the list of focal settings for the image source
+    def set_focus(self, focal_settings: list) -> None:
         pass
 
+    ##
+    # @brief capture - the method that captures the image from the image source. 
+    def capture(self) -> None:
+        """
+        Blocking
+        """
+        pass
+
+    ##
+    # @brief close - the method that closes the resources fo this image source.
     def close(self) -> None:
         pass
--- cslics_vision_processor/imaging/image_source_picam.py
+++ cslics_vision_processor/imaging/image_source_picam.py
@@ -4,8 +4,10 @@
 # Date:     2024-05-31
 
 import numpy
+import time
 from typing import Callable
 from logging import Logger
+from cslics_vision_processor.imaging.arducam_focuser import ArducamFocuser
 from cslics_vision_processor.imaging import ImageSource
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
@@ -23,15 +25,22 @@ class CallbackOutput(Output):
         self.callback_on_frame_encoded: Callable[[bytes], None] = callback_on_frame_encoded
 
     ##
-    # @brief outputframe - overloded method of the class picamera2.outputs.Output for recieving image frame data.
+    # @brief outputframe - overloaded method of the class picamera2.outputs.Output for recieving image frame data.
     # @param frame : the image data in bytes
     # @param keyframe : whether the frame is a keyframe (default True)
     # @param timestamp : the timestamp of the frame
     def outputframe(self, frame: bytes, keyframe=True, timestamp=None) -> None:
         self.callback_on_frame_encoded(frame)
 
-
+##
+# @brief class ImageSourcePiCam(ImageSource) - implements the interface 'ImageSource' of methods for pi-camera operations.
 class ImageSourcePiCam(ImageSource):
+
+    ##
+    # @brief __init__ - initialises this pi-camera operation instance.
+    # @param output_length_max : the maximum number of bytes in the image
+    # @param callback_on_frame_raw : the frame callback function
+    # @param callback_on_frame_encoded : the frame encoding callback function
     def __init__(self, output_length_max: int, callback_on_frame_raw: Callable[[numpy.ndarray], None], callback_on_frame_encoded: Callable[[bytes], None], logger: Logger):
         super().__init__(output_length_max, callback_on_frame_raw, callback_on_frame_encoded, logger.getChild(ImageSourcePiCam.__name__))
 
@@ -56,7 +65,28 @@ class ImageSourcePiCam(ImageSource):
         
         self.camera.encode_stream_name = 'main'
         self.camera.start_encoder(self.encoder)
+
+        self.focuser = ArducamFocuser(10)
+
+    ##
+    # @brief get_focal_parameters - gets a list of focal parameters from the pi-camera.
+    # @return list - a list of focal parameters
+    def get_focus(self) -> list:
+        self.camera.start()
+        print("GETTING LENS PARAMETERS")
+        print(self.focuser.get(self.focuser.OPT_FOCUS))
+        self.camera.stop()
+        return []
     
+    ##
+    # @brief set_focus - adjusts the focus of the pi-camera.
+    # @param focal_settings : the list of focal settings for the image source
+    def set_focus(self, focal_settings: list) -> None:
+        self.camera.start()
+        self.focuser.set(self.focuser.OPT_FOCUS, focal_settings[0])
+        self.camera.stop()
+    ##
+    # @brief capture - the method that starts the pi-camera, requests a frame, stops the camera, and captures the frame. 
     def capture(self) -> None:
         # If the camera is left on, it captures continuously
         self.camera.start()
@@ -65,6 +95,8 @@ class ImageSourcePiCam(ImageSource):
         self.callback_on_frame_raw(request.make_array('main'))
         request.release()
     
+    ##
+    # @brief close - the method closes the pi-camera and JPEG encoder.
     def close(self) -> None:
         self.camera.close()
         self.camera.stop_encoder()
