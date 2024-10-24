c34df1bd6070bcf0579a5e5e761c4b1ce4c441d9
Alec Tutin <a.tutin@qut.edu.au>
2024-10-24 14:13:37 +1000

    Improving similarity of functionality of ImageSourceStorageLocal to ImageSourcePicam

--- cslics_vision_processor/imaging/image_source_storage_local.py
+++ cslics_vision_processor/imaging/image_source_storage_local.py
@@ -3,50 +3,96 @@
 # Author:   Alec Tutin
 # Date:     2024-06-04
 
-import os, cv2, numpy
-from typing import Callable, Optional, List
+import cv2, time, numpy, os
+from cslics_vision_processor.imaging import ImageSource
 from logging import Logger
 from pathlib import Path
-from cslics_vision_processor.imaging import ImageSource
+from threading import Thread
+from typing import Callable, Optional, List
 
 class ImageSourceStorageLocal(ImageSource):
     def __init__(self, output_length_max: int, callback_on_frame_raw: Callable[[numpy.ndarray], None], callback_on_frame_encoded: Callable[[bytes], None], image_directory: str, logger: Logger):
         super().__init__(output_length_max, callback_on_frame_raw, callback_on_frame_encoded, logger.getChild(ImageSourceStorageLocal.__name__))
 
-        image_path: Path = Path(os.path.expanduser(image_directory))
-        self.images: List[Path] = []
-        self.image_index: int = 0
+        self.__is_running: bool = True
+        self.__is_streaming: bool = False
 
-        self.latest_path: Optional[Path] = None
+        image_path: Path = Path(os.path.expanduser(image_directory))
+        self.__images: List[Path] = []
+        self.__image_index: int = 0
 
         if image_path.exists():
             for ext in ['jpg', 'jpeg', 'png']:
                 for c in image_path.glob(f'*.{ext}'):
-                    self.images.append(c)
+                    self.__images.append(c)
         
-        self.images.sort()
+        self.__images.sort()
+
+        self.__stream_thread: Optional[Thread] = None
+
+    def __get_next_image_path(self) -> Path:
+        if self.__image_index >= len(self.__images):
+            self.__image_index = 0
+        
+        image_path: Path = self.__images[self.__image_index]
+        self.__image_index += 1
+
+        return image_path
+
+    def __stream_loop(self) -> None:
+        frame_delay: float = 1.0 / 10.0
+
+        self.logger.info('Starting stream...')
+
+        while self.__is_running and self.__is_streaming:
+            loop_start: float = time.time()
+            
+            image_path: Path = self.__get_next_image_path()
+
+            with image_path.open('rb') as file:
+                data: bytes = file.read()
+
+            self.callback_on_frame_encoded(data)
+
+            required_sleep: float = frame_delay - (time.time() - loop_start)
+            
+            if required_sleep <= 0.0:
+                time.sleep(0.01)
+            else:
+                time.sleep(required_sleep)
+
+        self.logger.info('Stream stopped...')
+
+    def start(self) -> None:
+        if self.__stream_thread is not None:
+            return
+        
+        self.__is_streaming = True
+
+        self.__stream_thread = Thread(target=self.__stream_loop, name=f'{ImageSourceStorageLocal.__name__}.__stream_loop')
+        self.__stream_thread.start()
+
+    def stop(self) -> None:
+        self.__is_streaming = False
+
+        if self.__stream_thread is None:
+            return
+
+        self.__stream_thread.join()
+        self.__stream_thread = None
     
     def capture(self, mode: int) -> None:
-        # Updated to mimic behaviour of image_source_picam.py
-        
-        if self.image_index >= len(self.images):
-            self.image_index = 0
-        
-        self.latest_path = self.images[self.image_index]
-        self.image_index += 1
+        image_path: Path = self.__get_next_image_path()
 
-        self.logger.info(f'Loading image from: {self.latest_path}')
+        self.logger.info(f'Loading capture image from: {image_path}')
 
-        buffer_original: numpy.ndarray = numpy.fromfile(self.latest_path, dtype=numpy.uint8)
-        image_original: numpy.ndarray = cv2.imdecode(buffer_original, cv2.IMREAD_COLOR)
+        image_original: numpy.ndarray = cv2.imdecode(numpy.fromfile(image_path, dtype=numpy.uint8), cv2.IMREAD_COLOR)
 
         if mode == 1:
             self.callback_on_frame_raw(image_original)
         else:
             image_model_size: numpy.ndarray = ImageSourceStorageLocal.resize_to_max_length(image_original, self.output_length_max)
             self.callback_on_frame_raw(cv2.cvtColor(image_model_size, cv2.COLOR_RGB2BGR))
-        
-        self.callback_on_frame_encoded(buffer_original.tobytes())
 
     def resize_to_max_length(source: numpy.ndarray, length: int) -> numpy.ndarray:
         (height, width, _) = source.shape
@@ -64,3 +110,6 @@ class ImageSourceStorageLocal(ImageSource):
     
     def get_dof_volume(self) -> float:
         return 3
+    
+    def close(self) -> None:
+        self.stop()
